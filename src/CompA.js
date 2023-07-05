f25c12c95e31e1368da4e93c667d209620ab5c32
aaushi <aaushi1512@gmail.com>
2023-07-05 06:42:43 +0530

    context api fix errors

--- src/CompA.js
+++ src/CompA.js
@@ -1,10 +1,10 @@
 import React from 'react'
 import CompB from './CompB'
 
-const CompA = () => {
+const compA = () => {
   return (
    <CompB/>
   )
 }
 
-export default CompA
+export default compA
