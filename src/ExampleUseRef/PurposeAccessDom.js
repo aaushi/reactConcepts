2e0b6e7d0690ced264ad4737b9a0f391c3295d4c
aaushi <aaushi1512@gmail.com>
2023-07-13 06:19:12 +0530

    useRef example

--- src/ExampleUseRef/PurposeAccessDom.js
+++ src/ExampleUseRef/PurposeAccessDom.js
@@ -1,9 +1,18 @@
-import React from 'react'
+import React, { useRef } from 'react'
 
 const PurposeAccessDom = () => {
+    const inputRef=useRef(null);
+    const handleSubmit=()=>{
+        console.log(inputRef.current.value)
+        inputRef.current.style.backgroundColor="yellow"
+    }
   return (
-    <div>PurposeAccessDom</div>
-  )
+    <>
+      <div>use to access DOM directly</div>
+      <input  ref={inputRef}/>
+      <button onClick={()=>handleSubmit()}>Submit</button>
+    </>
+  );
 }
 
 export default PurposeAccessDom
--- src/ExampleUseRef/PurposeMutantVariable.js
+++ src/ExampleUseRef/PurposeMutantVariable.js
@@ -1,9 +1,20 @@
-import React from 'react'
+import React,{useEffect, useRef, useState} from 'react'
 
 const PurposeMutantVariable = () => {
+const [name,setName]=useState();
+const count=useRef(0);
+useEffect(()=>{
+    count.current=count.current+1;
+})
   return (
-    <div>PurposeMutantVariable</div>
-  )
+    <>
+      <div>
+        It creates a mutable variable which will not re render the components
+      </div>
+      <input value={name} onChange={(e) => setName(e.target.value)}></input>
+      <p> count:{count.current}</p>
+    </>
+  );
 }
 
 export default PurposeMutantVariable
