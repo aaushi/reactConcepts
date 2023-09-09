eb5b377b0e500571c876bbe452db247a5b5b7d6d
aaushi <aaushi1512@gmail.com>
2023-09-09 22:53:21 +0530

    Login validation

--- src/Questions/loginForm/LoginForm.js
+++ src/Questions/loginForm/LoginForm.js
@@ -6,47 +6,42 @@ const LoginForm = () => {
     password: "",
     email: "",
   });
-  const [formErrors,setFormErrors]=useState({});
+  const [formErrors, setFormErrors] = useState({});
   const [isSubmit, setIsSubmit] = useState(false);
-  const handleSubmit=(e)=>{
+  const handleSubmit = (e) => {
     e.preventDefault();
-    console.log(e)
+    console.log(e);
     setFormErrors(validateForm(formValue));
-    console.log(formErrors)
-    setIsSubmit(true)
-  }
-  /* useEffect(()=>{
-    console.log(formErrors)
-    if(Object.keys(formErrors).length===0 && isSubmit){
-        console.log(formValue);
+    console.log(formErrors);
+    setIsSubmit(true);
+  };
+  useEffect(() => {
+    console.log(formErrors);
+    if (Object.keys(formErrors).length === 0 && isSubmit) {
+      console.log(formValue);
     }
-
-  },[formErrors]) */
+  }, [formErrors]);
   const validateForm = (formValue) => {
-    let errors={};
+    let errors = {};
     console.log(formValue);
     const regexEmail = new RegExp("[a-z0-9]+@[a-z]+.[a-z]{2,3}");
     const regexUsername = new RegExp("^[a-zA-Z]*$");
-    if(!formValue.username ){
-        errors.username="Username is required";
+    if (!formValue.username) {
+      errors.username = "Username is required";
     }
     if (!regexUsername.test(formValue.username)) {
       errors.username = "Username is not Valid";
     }
-    if(!formValue.password ){
-        errors.password="password is required";
-    }
-    else if (formValue.password.length<4) {
+    if (!formValue.password) {
+      errors.password = "password is required";
+    } else if (formValue.password.length < 4) {
       errors.password = "password is not valid";
     }
-    if(!formValue.email){
-        errors.email="Email is required";
-    }
-    else if (!regexEmail.test(formValue.email)) {
+    if (!formValue.email) {
+      errors.email = "Email is required";
+    } else if (!regexEmail.test(formValue.email)) {
       errors.email = "Email is not Valid";
     }
-  
-    //console.log(errors);
     return errors;
   };
   return (
@@ -62,6 +57,7 @@ const LoginForm = () => {
         <p style={{ color: "red" }}>{formErrors.username}</p>
         <label>Password:</label>
         <input
+          type="password"
           values={formValue.password}
           onChange={(e) =>
             setFormValues({ ...formValue, password: e.target.value })
